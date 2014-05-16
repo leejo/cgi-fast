05efd3e3ec9e73f6d1fa978dcaf22b065d581bdd
Lee Johnson <lee@givengain.ch>
2014-05-16 13:53:11 +0200

    remove all old CGI.pm entries from Changes
    and add not that these can be see in the CGI.pm Changes file for
    version prior to 3.65_01

--- lib/CGI/Fast.pm
+++ lib/CGI/Fast.pm
@@ -20,7 +20,7 @@ local $^W = 1;
 # wish, but if you redistribute a modified version, please attach a note
 # listing the modifications you have made.
 
-$CGI::Fast::VERSION='1.10_01';
+$CGI::Fast::VERSION='1.10_02';
 
 use CGI;
 use FCGI;
